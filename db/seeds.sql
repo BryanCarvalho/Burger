642ae06bd603e43d5702c07c65af64c07024b456
Bryan C <bryan.carvalho.design@gmail.com>
2018-11-04 17:37:47 -0500

    Setup burger tables and queries

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheeseburger', FALSE), ('Double Cheeseburger', FALSE), ('Triple Cheeseburger', FALSE);
+
+SELECT * FROM burgers; 
