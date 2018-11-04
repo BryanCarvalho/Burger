c0e50bc6e49dd5a9a74b188003aba3d53bd9ee2d
Bryan C <bryan.carvalho.design@gmail.com>
2018-11-04 18:33:12 -0500

    Adding JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,27 @@
 
 let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
+    if (err) {
+        console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+        return;
+    }
+    console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
-module.exports = connection; 
+module.exports = connection;
